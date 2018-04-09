6ee063b29ab2a2a2e4be194d60a618fef766bf00
Frankie Robertson <frankie@robertson.name>
2018-04-09 20:07:52 +0300

    Update to fixed dep_search

--- base/setup.sh
+++ base/setup.sh
@@ -1,4 +1,4 @@
-git clone --branch working-setlib https://github.com/frankier/dep_search.git
+git clone --branch fix-9-04 https://github.com/frankier/dep_search.git
 cd dep_search
 git submodule init
 git submodule update
